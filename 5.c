02a45e20a1f7840d3fca665d04daa7ec4be2593e
ounyoung4747 <128661268+ounyoung4747@users.noreply.github.com>
2023-04-27 11:52:37 +0900

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+int x; 
+int y; 
+int a, b, c, d; 
+x = 20; 
+y = 10; 
+a = x + y; 
+b = x - y; 
+c = x * y; 
+d = x / y;
+
+printf("두수의 합: %d\n", a); 
+printf("두수의 차: %d\n", b);
+printf("두수의 곱: %d\n", c); 
+printf("두수의 몫: %d\n", d); 
+return 0;
+}
---
+++ 9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+int x, y;
+printf("두개의 정수를 입력하시오: ");
+scanf("%d %d", &x, &y);
+printf("%d && %d의 결과값: %d", x, y, x && y);
+printf("%d || %d의 결과값: %d", x, y, x || y);
+printf("!%d의 결과값: %d", x, !x);
+return 0;
+}
